6eb491ccffa039e1109128937c62c3df9cdc7c5f
Divyanshi thapa <newone1804@gmail.com>
2025-08-07 17:28:17 +0000

    Add files via upload

--- c_scrap.c
+++ c_scrap.c
@@ -1,33 +1,83 @@
 #include <stdio.h>
 
-int calculateSum(int x, int y) {
-  return x + y;
-}
 
-int main() {
-  // Create an array
-  //int resultArr[6];
-  int num1 ;
-  int num2;
+/*
+  Gross Salary Calculator
+*/
+// int main(void){
+//     int salary=0 , allowance =0, rent=0, total_salary = 0;
+//     printf("Type your final salary:");
+//     scanf("%d", &salary);
+//     allowance = (salary * 40) /100;
+//     rent = (salary * 20) /100;
+//     total_salary = allowance + rent + salary;
 
-  int result;
-    //int length = sizeof(resultArr) / sizeof(resultArr[0]);
-  
+//     printf("You gross salary: %d", total_salary);
+//     return 0;
 
-  // Call the function with different arguments and store the different results in the array
-//
-    printf("Choose a num: ");
-    scanf("%d", &num1);
-    printf("Choose another num:");
-    scanf("%d", &num2);
+
+
+//}
+
+
+/*
+
+   Percentage Calculator
+*/
+// int main(void){
+//     float i=0,sub1, sub2, sub3, sub4, sub5, aggregate_marks, percentage;
+
+//     printf("Type your marks :\n");
+//     scanf("%f\n%f\n%f\n%f\n%f",&sub1, &sub2, &sub3, &sub4, &sub5 );
+//     aggregate_marks = sub1+sub2+sub3+sub4+sub5 ;
+//     percentage = (aggregate_marks/ 500) * 100;
+//     printf("Your total marks: %.0f/500\nYour percentage: %.2f",aggregate_marks, percentage);
+//     return 0;
+
+
+// }
+
+
+/*
+Area And Perimeter Calculator
+*/
+
+// int main(void){
+//     char choice[20] = "";
+//     int length =0 , breadth=0, radius = 0, area = 0, perimeter = 0, circumference = 0 ;
+//     printf("You choose rectangle or circle?\n");
+//     scanf("%s", &choice);
+//     //printf(choice);
+
+//     if (strcmp(choice, "rectangle") == 0){
+//         printf("Type length of rectangle:");
+//         scanf("%d", &length);
+//         printf("Type breadth of rectangle:");
+//         scanf("%d", &breadth);
+//         area = length * breadth;
+//         perimeter = 2*(length + breadth);
+//         printf("Area of this rect: %d\nPerimeter:%d", area, perimeter);
+        
+
+//     }
+
+//     else if (strcmp(choice, "circle") == 0) {
+//         printf("Type radius of circle:");
+//         scanf("%d", &radius);
+//         circumference = 2 * 3.14 * radius;
+//         printf("Circumference of this circle: %d", circumference);
+        
+//        }
+
+//          return 0;
+//     }
+   
+
+/*
+Paper Sizes
+*/
+
+
+
     
- 
-  //for (int i = 0; i < length; i++) {
-
- 	//resultArr[i] = calculateSum(num1, num2);
-  result = calculateSum(num1, num2);
-  printf("Result is = %d", result);
-  
-
-  return 0;
-}
+   
