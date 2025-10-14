39f0472c5fb3804e625d3da6fa39fbafd9bf84a6
boa3444 <newone1804@gmail.com>
2025-10-14 19:12:56 +0000

    C

---
+++ practice.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+//#define ARANGE ( a > 25 && a<50)
+//void g();
+//static int y ;
+
+#define MULT(x) ( x + 1)
+int main()
+{
+//	int i = 0 ;
+//	for ( i = 0 ; i <= 50000; i++)
+//	{
+//		printf("%d" , i );
+//	}
+//
+//	float x = 13.5;
+//	printf("%f" , x);
+//	return 0 ;
+//	int x , y , s = 2;
+//	s *= 3;
+//	y = f(s);
+	//	x = g(s);
+//	printf("%d %d %d" , s , y ,x);
+//	static int count = 5;
+//	printf("%d" , count--);
+//	if ( count != 0)
+//		main();
+//	int j ;
+//	for ( int i = 1 ;i < 5; i ++)
+//	{
+//	j = g(i);
+//	printf("%d" , j);
+//	}
+//	g();
+////	g();
+//
+//
+//	return 0 ;
+//
+//void g()
+//{
+//	static int v = 0;
+//	register int j = 0;
+//	auto int k = 0 ;
+//	v++;j++;k++;
+//	printf("%d %d %d " , v , j , k);
+////}
+//	printf("%d" , y);
+//	return 0 ;
+//
+//	int b = 39;
+//	if ( ARANGE)
+//		printf("yes");
+//	return 0 ;
+//}
+	int hi = 38;
+	printf("%c" , MULT(hi));
+	return 0 ;
+}
