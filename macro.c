2e6d6aef892a5608ae8e5ae1b84c4cc51e56b0de
boa3444 <newone1804@gmail.com>
2025-12-03 05:22:05 +0000

    C

---
+++ macro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//
+//#define MAX(a,b,c) ((a>b) ? ((a>c) ? a : c ) : ((b>c) ? b : c))
+//#define MIN(a,b,c) ((a<b) ? ((a<c) ? a : c ) : ((b<c) ? b : c))
+//
+//int main()
+//{
+//	int num1 , num2 , num3 ;
+//	scanf("%d %d %d" , &num1 , &num2 , &num3);
+//	int max = MAX(num1 , num2 , num3);
+//	int min = MIN(num1 , num2 , num3);
+//	printf("Max: %d\n Min: %d", max , min);
+//	return 0 ;
+//}
+
+//#define ARRANGE (a>25 && a<50)
+//
+//int main()
+//{
+//	int b=30;
+//	printf("%d", ARRANGE);
+//	return 0 ;
+	
+
+}
