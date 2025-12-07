f8542c725c88352c357ed44d7ae4001aa0d06804
boa3444 <newone1804@gmail.com>
2025-12-07 16:14:40 +0000

    C

---
+++ aes_cbc.c
@@ -0,0 +1,402 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <openssl/rand.h>
+
+
+#define BLOCKSIZE 16
+const unsigned char sbox[256] = {
+    0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5,
+    0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+    0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0,
+    0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+    0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC,
+    0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+    0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A,
+    0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+    0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0,
+    0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+    0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B,
+    0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+    0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85,
+    0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+    0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5,
+    0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+    0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17,
+    0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+    0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88,
+    0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+    0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C,
+    0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+    0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9,
+    0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+    0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6,
+    0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E,
+    0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94,
+    0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68,
+    0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
+};
+uint8_t Rcon[11][4] = {
+	{0x00, 0x00, 0x00, 0x00},
+	{0x01, 0x00, 0x00, 0x00},
+	{0x02, 0x00, 0x00, 0x00},
+	{0x04, 0x00, 0x00, 0x00},
+	{0x08, 0x00, 0x00, 0x00},
+	{0x10, 0x00, 0x00, 0x00},
+	{0x20, 0x00, 0x00, 0x00},
+	{0x40, 0x00, 0x00, 0x00},
+	{0x80, 0x00, 0x00, 0x00},
+	{0x1B, 0x00, 0x00, 0x00},
+	{0x36, 0x00, 0x00, 0x00}
+	};
+
+uint8_t key_schedule[11][16]; //11 keys with 16 elements(4words)
+
+
+
+
+unsigned char * pkcs_pad ( char * , int* );
+void SubBytes(uint8_t state_matrix[4][4]);
+void ShiftRows(uint8_t state_matrix[4][4]);
+uint8_t Gf_mult( uint8_t , uint8_t);
+void MixColumns(uint8_t state_matrix[4][4]);
+void g(uint8_t word[4],int round);
+void KeyExpansion(uint8_t key_matrix[4][4]);//no of round in int
+void AddRoundKey(uint8_t state_matrix[4][4], uint8_t roundkey[16]);
+
+
+
+int main()
+{
+	unsigned char *user_input, buffer[1000]; //simple input of user stored in 1D array
+	uint8_t state_matrix[4][4]; //the state matrix declared
+	int i=0; // kahi bhi use krlo ( for index) 
+
+	//Taking input and storing it in user_input
+	fgets(buffer,sizeof(buffer),stdin);
+	int len_input = strlen(buffer);
+	user_input = malloc( len_input );
+	memcpy(user_input , buffer , len_input);
+
+
+	//Applying PKCS#7 to user_input:
+	unsigned char * padded_input = pkcs_pad(user_input,&len_input);
+
+
+	//Set elements in state matrix:
+	i=0;
+	for ( int c=0;c<4;c++)
+	{
+		for ( int r=0;r<4;r++)
+		{
+			state_matrix[r][c] = padded_input[i];
+			i++;
+		}
+	}
+
+	//Printing the state matrix
+	//	for ( int r=0;r<4;r++)
+	//	{
+	//		for ( int c=0;c<4;c++)
+	//		{
+	//			printf("%02x " , state_matrix[r][c]);
+	//		}
+	//		printf("\n");
+	//	}
+
+
+
+	unsigned char iv[16]; // Initializing vector
+	//IV generation
+	if (!RAND_bytes(iv, sizeof(iv))) 
+	{
+		fprintf(stderr,"IV generation not success\n"); //fprintf is better for error handling
+	}
+
+//	for ( int r=0;r<16;r++)
+//	{
+//		printf("%d " , iv[r]);
+//	}
+
+	// XORed state_matrix with IV
+	i=0;
+	for ( int c=0;c<4;c++)
+	{
+		for ( int r=0;r<4;r++)
+		{
+			state_matrix[r][c] ^= iv[i];
+			i++;
+		}
+	}
+
+
+
+	//Key wala part
+	//Creating the original key (1d array)
+	uint8_t original_key[16];
+	if ( !RAND_bytes(original_key, sizeof(original_key)) )
+	{
+		printf("Couldn't create the key");
+		return 1;
+	}
+
+	i=0;
+	uint8_t key_matrix[4][4];
+	for ( int c=0;c<4;c++)
+	{
+		for ( int r=0;r<4;r++)
+		{
+			key_matrix[r][c] = original_key[i];
+			i++;
+		}
+	}
+
+	KeyExpansion(key_matrix); //adds round keys to key_schedule[11][16]
+
+
+	AddRoundKey(state_matrix, key_schedule[0]);
+	for (int round = 1; round <= 9; round++)
+	{
+	    SubBytes(state_matrix);
+	    ShiftRows(state_matrix);
+	    MixColumns(state_matrix);
+	    AddRoundKey(state_matrix, key_schedule[round]);
+	}
+	SubBytes(state_matrix);
+	ShiftRows(state_matrix);
+	AddRoundKey(state_matrix, key_schedule[10]);
+	
+	//store the first ciphertxt in a block
+	uint8_t cipher_block[4][4];
+	for ( int c=0;c<4;c++)
+	{
+		for ( int r=0;r<4;r++)
+		{
+			cipher_block[r][c]= state_matrix[r][c];
+		}
+	} 
+
+	return 0 ;
+
+}
+
+//ALL FUNCTIONS
+//padding :
+unsigned char *  pkcs_pad(char * array , int *len)
+{
+	int pad_len = BLOCKSIZE - ( *len % BLOCKSIZE);
+	if ( pad_len == 0 )
+		pad_len = 16;
+	array = realloc( array,(*len)+pad_len);
+	for ( int i=0;i<pad_len;i++)
+	{
+		array[*len + i]=pad_len;
+	}
+	*len += pad_len;
+	return array; //since here array is LOCAL and we used the realloc
+}
+
+void SubBytes( uint8_t state_matrix[4][4])
+{	// SubBytes
+	for ( int c=0;c<4;c++)
+	{
+		for ( int r=0;r<4;r++)
+		{
+			state_matrix[r][c] = sbox[state_matrix[r][c]];
+		}
+	}
+}
+
+void ShiftRows( uint8_t state_matrix[4][4])
+{
+	int r=1;
+	while ( r<4)
+	{
+		for ( int times=0;times<r;times++)
+		{
+			for ( int c = 0;c< 3;c++)
+			{
+				//unsigned char temp1=state_matrix[r][c];
+				unsigned char temp = state_matrix[r][c];
+				state_matrix[r][c]= state_matrix[r][c+1];
+				state_matrix[r][c+1]=temp;
+			}
+		}
+		r++;
+	}
+}
+
+
+/* fixed matrix[4][4]=
+	{
+	    {0x02, 0x03, 0x01, 0x01},
+	    {0x01, 0x02, 0x03, 0x01},
+	    {0x01, 0x01, 0x02, 0x03},
+	    {0x03, 0x01, 0x01, 0x02}
+	};
+*/
+
+uint8_t Gf_mult( uint8_t byte , uint8_t mult_with)
+{
+	if ( mult_with == 1)
+		return byte;
+	if ( mult_with == 2)
+	{
+		uint8_t temp = (byte << 1) & 0xff; // 0xff for masking the overflow of 8 bits
+		if ( (byte >> 7 ) == 1)
+		{
+			return temp ^ 0x1b;
+		}
+		else
+			return temp;
+	}
+
+	if ( mult_with == 3)
+	{
+		uint8_t temp = ( Gf_mult(byte, 2));
+		return (temp ^ byte);
+	}
+	return 0 ;
+}
+
+void MixColumns(uint8_t state_matrix[4][4])
+{
+	for ( int c=0; c< 4 ;c++)
+	{
+		uint8_t temp1 = (Gf_mult(state_matrix[0][c], 0x02)) ^ (Gf_mult(state_matrix[1][c] , 0x03))^ (Gf_mult(state_matrix[2][c], 0x01)) ^ (Gf_mult(state_matrix[3][c], 0x01));
+
+		uint8_t temp2 = (Gf_mult(state_matrix[0][c], 0x01)) ^ (Gf_mult(state_matrix[1][c] , 0x02))^ (Gf_mult(state_matrix[2][c], 0x03)) ^ (Gf_mult(state_matrix[3][c], 0x01));
+
+		uint8_t temp3 = (Gf_mult(state_matrix[0][c], 0x01)) ^ (Gf_mult(state_matrix[1][c] , 0x01))^ (Gf_mult(state_matrix[2][c], 0x02)) ^ (Gf_mult(state_matrix[3][c], 0x03));
+
+		uint8_t temp4 = (Gf_mult(state_matrix[0][c], 0x03)) ^ (Gf_mult(state_matrix[1][c] , 0x01))^ (Gf_mult(state_matrix[2][c], 0x01)) ^ (Gf_mult(state_matrix[3][c], 0x02));
+
+
+		state_matrix[0][c] = temp1;
+		state_matrix[1][c]=temp2;
+		state_matrix[2][c]=temp3;
+		state_matrix[3][c]=temp4;
+	}
+}
+
+void g( uint8_t word[4], int round)
+{
+	//rotate word
+	//RotWord
+/*	for ( int i =0;i<3; i++)*/
+/*	{*/
+/*		int temp = word[i];*/
+/*		word[i]=word[i+1];*/
+/*		word[i+1]=temp;*/
+/*	}*/
+	uint8_t temp = word[0];
+	word[0] = word[1];
+	word[1] = word[2];
+	word[2] = word[3];
+	word[3] = temp;
+
+	//Sbox substitution
+	//SubWord
+	for ( int i=0;i<4;i++)
+	{
+		word[i] = sbox[word[i]];
+	}
+	
+	
+	//Rcon
+	//just XOR
+	
+	word[0] ^= Rcon[round][0]; // other bytes xor with 0x00 so unchanged...
+	
+
+}
+void KeyExpansion(uint8_t original_key[4][4])
+{
+	//4 words input
+	//key schedule have 4 words in each of its rows ( 16 elements )
+	
+	// manipulate data word-wise but store it element by element
+	
+	//to make it easier i chose to store the column-wise words as row-based words
+	//then transfer these words into key_schedule for easier accessiblitity for each round
+	uint8_t key_matrix[44][4];
+	int key_r=0,key_c=0;
+	int round = 1;
+	for ( int c=0;c<4;c++)
+	{
+		for ( int r=0;r<4;r++)
+		{
+			key_matrix[key_r][key_c]= original_key[r][c];
+			key_c++;
+		}
+		key_r++;
+		key_c=0;
+	}
+
+	for ( key_r = 4;key_r<44;key_r++)
+	{
+		if ( key_r % 4 !=0)
+		{
+			//1 word in each row of key_matrix
+			for ( int col=0;col<4;col++)
+			{
+				key_matrix[key_r][col] = key_matrix[key_r-1][col] ^ key_matrix[key_r-4][col];
+			}
+		}
+		
+		else if ( key_r % 4 == 0 )
+		{
+			uint8_t temp_for_g[4];
+			for (int col = 0; col < 4; col++)
+			{
+				temp_for_g[col] = key_matrix[key_r-1][col];
+			}
+			
+			g(temp_for_g, round);
+			round++;
+			
+			for ( int col=0;col<4;col++)
+			{
+				key_matrix[key_r][col] = temp_for_g[col] ^ key_matrix[key_r-4][col];
+			}
+		}
+	}
+
+	 //key_schedule[11][16]
+	 key_r=0;
+	 key_c=0;
+	 for ( int row = 0 ;row<11;row++)
+	 {
+	 	for ( int col =0;col<16;col++)
+	 	{
+	 		key_schedule[row][col] = key_matrix[key_r][key_c];
+	 		key_c++;
+	 		if ( key_c % 4 ==0)
+	 		{
+	 			key_c=0;
+	 			key_r++;
+	 		}
+	 	}
+	 	
+	 }
+
+
+}
+
+//byte to byte
+void AddRoundKey( uint8_t state_matrix[4][4] , uint8_t round_key[16])
+{
+	int i=0;
+	for ( int c=0;c< 4;c++)
+	{
+		for ( int r = 0 ;r<4;r++)
+		{
+			state_matrix[r][c] ^= round_key[i];
+			i++;
+		}
+	}
+}
